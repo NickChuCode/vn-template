5702f898fa8a5b7b95998cd2c9bf3fac935d8225
NickChuCode <nickchu15@outlook.com>
2018-12-19 11:23:51 +0800

    add notes in build.js

--- README.md
+++ README.md
@@ -43,4 +43,23 @@ npm test
 - --config 后面跟配置文件，可以看到，主配置文件是build/webpack.dev.conf.js
 关于webpack-dev-server的自动刷新模式，可以看[这里](https://github.com/NickChuCode/vn-template/issues/1)
 ## webpack.dev.conf.js
+> webpack开发环境配置
+
+这个文件主要做了以下几件事情：
+1. 引入各种依赖，同时也引入了config文件夹下的变量和配置，和一个工具函数build/utils.js，
+2. 合并build/webpack.base.conf.js配置文件，
+3. 配置开发环境一些devServer，plugin等配置，
+4. 最后导出了一个Promise，根据配置的端口，寻找可用的端口来启动服务。
+
+具体的注释看[这里](https://github.com/NickChuCode/vn-template/blob/master/build/webpack.dev.conf.js)
+
+## build.js
+> npm run build 指定的运行文件
+
+这个文件主要做了以下几件事情：
+1、引入build/check-versions文件，检查node和npm的版本，
+2、引入相关插件和配置，其中引入了webpack生产环境的配置build/webpack.prod.conf.js，
+3、先控制台输出loading，删除dist目录下的文件，开始构建，构建失败和构建成功都给出相应的提示信息。
+
+具体注释看[这里](https://github.com/NickChuCode/vn-template/blob/master/build/build.js)
 
--- build/build.js
+++ build/build.js
@@ -1,22 +1,29 @@
 'use strict'
+// 直接运行，检查node npm 版本
 require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
+// 命令行中的loading效果
 const ora = require('ora')
+// // 删除文件或文件夹，实现统一的rm -rf
 const rm = require('rimraf')
 const path = require('path')
+// 控制台输出彩色文本，更多查看：https://github.com/chalk/chalk
 const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
+// 引入生产环境webpack配置
 const webpackConfig = require('./webpack.prod.conf')
-
+// 控制台输入开始构建loading
 const spinner = ora('building for production...')
 spinner.start()
 
+// 删除原有构建输出的目录文件 这里是dist 和 static
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
   webpack(webpackConfig, (err, stats) => {
+    // 关闭loading
     spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
@@ -27,11 +34,13 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       chunkModules: false
     }) + '\n\n')
 
+    // 如果有错，控制台输出构建失败
     if (stats.hasErrors()) {
       console.log(chalk.red('  Build failed with errors.\n'))
       process.exit(1)
     }
 
+    // 控制台输出构建成功相关信息
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
